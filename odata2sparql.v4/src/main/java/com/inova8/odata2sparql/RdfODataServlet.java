f7603404beaa6c9c424d7915b83e2ec179f5d2be
Peter <peter.lawrence@inova8.com>
2017-10-22 18:36:56 +0100

    Added simple home page

--- odata2sparql.v4/src/main/java/com/inova8/odata2sparql/RdfODataServlet.java
+++ odata2sparql.v4/src/main/java/com/inova8/odata2sparql/RdfODataServlet.java
@@ -1,14 +1,20 @@
 package com.inova8.odata2sparql;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.olingo.commons.api.edmx.EdmxReference;
 import org.apache.olingo.server.api.OData;
 import org.apache.olingo.server.api.ODataHttpHandler;
@@ -28,65 +34,82 @@ import com.inova8.odata2sparql.SparqlProcessor.SparqlPrimitiveValueProcessor;
 import com.inova8.odata2sparql.SparqlProcessor.SparqlReferenceCollectionProcessor;
 import com.inova8.odata2sparql.SparqlProcessor.SparqlReferenceProcessor;
 
-
 public class RdfODataServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private final Logger log = LoggerFactory.getLogger(RdfODataServlet.class);
-	static private RdfEdmProviders rdfEdmProviders=null;
+	static private RdfEdmProviders rdfEdmProviders = null;
 
-
-	protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException,
-			IOException {
+	protected void service(final HttpServletRequest req, final HttpServletResponse resp)
+			throws ServletException, IOException {
 		try {
-			if(rdfEdmProviders==null){
-				 rdfEdmProviders = new RdfEdmProviders(this.getInitParameter("repositoryFolder"),this.getInitParameter("repositoryUrl"));
+			if (rdfEdmProviders == null) {
+				rdfEdmProviders = new RdfEdmProviders(this.getInitParameter("repositoryFolder"),
+						this.getInitParameter("repositoryUrl"));
 			}
-			String service = req.getPathInfo().split("/")[1];
-			if (service.equals(RdfConstants.RESET)) {
-				log.info(RdfConstants.RESET +  " requested: " + req.getPathInfo().split("/")[2]);
-				rdfEdmProviders.reset(req.getPathInfo().split("/")[2]);
-				simpleResponse(req,resp,RdfConstants.RESET +  ": " + req.getPathInfo().split("/")[2] );
-			} else if (service.equals(RdfConstants.RELOAD )) {
-				log.info(RdfConstants.RELOAD +  " requested");
-				rdfEdmProviders.reload();
-				simpleResponse(req,resp,RdfConstants.RELOAD );
-			} else {
-				//Find provider matching service name			
-				RdfEdmProvider rdfEdmProvider = rdfEdmProviders.getRdfEdmProvider(service);
-				// create odata handler and configure it with CsdlEdmProvider and Processor
-				if (rdfEdmProvider != null) {
-					OData odata = OData.newInstance();
-					ServiceMetadata edm = odata.createServiceMetadata(rdfEdmProvider, new ArrayList<EdmxReference>());
-					ODataHttpHandler handler = odata.createHandler(edm);
-					//Reserve first parameter for either the service name or a$RESET. $RELOAD
-					handler.setSplit(1);
-					handler.register(new SparqlEntityCollectionProcessor(rdfEdmProvider));
-					handler.register(new SparqlEntityProcessor(rdfEdmProvider));
-					handler.register(new SparqlPrimitiveValueProcessor(rdfEdmProvider));
-					handler.register(new SparqlDefaultProcessor());
-					handler.register(new SparqlReferenceCollectionProcessor(rdfEdmProvider));
-					handler.register(new SparqlReferenceProcessor(rdfEdmProvider));
-					handler.register(new SparqlBatchProcessor(rdfEdmProvider));
-					//handler.register(new SparqlErrorProcessor());
-					log.info(req.getMethod() + ": "+ req.getPathInfo()+" Query: "+req.getQueryString());
-					// let the handler do the work
-					handler.process(req, resp);
+			if (req.getPathInfo() != null && (!req.getPathInfo().equals("/"))) {
+				String service = req.getPathInfo().split("/")[1];
+				if (service.equals(RdfConstants.RESET)) {
+					log.info(RdfConstants.RESET + " requested: " + req.getPathInfo().split("/")[2]);
+					rdfEdmProviders.reset(req.getPathInfo().split("/")[2]);
+					simpleResponse(req, resp, RdfConstants.RESET + ": " + req.getPathInfo().split("/")[2]);
+				} else if (service.equals(RdfConstants.RELOAD)) {
+					log.info(RdfConstants.RELOAD + " requested");
+					rdfEdmProviders.reload();
+					simpleResponse(req, resp, RdfConstants.RELOAD);
 				} else {
-					throw new OData2SparqlException("No service matching " + service + " found");
+					//Find provider matching service name			
+					RdfEdmProvider rdfEdmProvider = rdfEdmProviders.getRdfEdmProvider(service);
+					// create odata handler and configure it with CsdlEdmProvider and Processor
+					if (rdfEdmProvider != null) {
+						OData odata = OData.newInstance();
+						ServiceMetadata edm = odata.createServiceMetadata(rdfEdmProvider,
+								new ArrayList<EdmxReference>());
+						ODataHttpHandler handler = odata.createHandler(edm);
+						//Reserve first parameter for either the service name or a$RESET. $RELOAD
+						handler.setSplit(1);
+						handler.register(new SparqlEntityCollectionProcessor(rdfEdmProvider));
+						handler.register(new SparqlEntityProcessor(rdfEdmProvider));
+						handler.register(new SparqlPrimitiveValueProcessor(rdfEdmProvider));
+						handler.register(new SparqlDefaultProcessor());
+						handler.register(new SparqlReferenceCollectionProcessor(rdfEdmProvider));
+						handler.register(new SparqlReferenceProcessor(rdfEdmProvider));
+						handler.register(new SparqlBatchProcessor(rdfEdmProvider));
+						//handler.register(new SparqlErrorProcessor());
+						log.info(req.getMethod() + ": " + req.getPathInfo() + " Query: " + req.getQueryString());
+						// let the handler do the work
+						handler.process(req, resp);
+					} else {
+						throw new OData2SparqlException("No service matching " + service + " found");
+					}
 				}
+			} else {
+				htmlResponse(req, resp, "/index.html");
 			}
 		} catch (RuntimeException | OData2SparqlException e) {
 			log.error("Server Error occurred in ExampleServlet", e);
 			throw new ServletException(e);
 		}
 	}
-	private void simpleResponse(final HttpServletRequest req, final HttpServletResponse resp, String textResponce) throws IOException{
-		  Scanner scanner = new Scanner(req.getInputStream());
-		  StringBuilder sb = new StringBuilder();
-		  while (scanner.hasNextLine()) {
-		    sb.append(scanner.nextLine());
-		  }
-		  resp.getOutputStream().println(textResponce);
-		  scanner.close();
+
+	private void htmlResponse(HttpServletRequest req, HttpServletResponse resp, String textResponse)
+			throws IOException {
+		try {
+			InputStream in = getServletContext().getResourceAsStream(textResponse);
+			String contents = IOUtils.toString(in);
+			simpleResponse(req, resp, contents);
+		} catch (Exception e) {
+			simpleResponse(req, resp, "odata2sparql.v4");
+		}
+	}
+
+	private void simpleResponse(final HttpServletRequest req, final HttpServletResponse resp, String textResponse)
+			throws IOException {
+		Scanner scanner = new Scanner(req.getInputStream());
+		StringBuilder sb = new StringBuilder();
+		while (scanner.hasNextLine()) {
+			sb.append(scanner.nextLine());
+		}
+		resp.getOutputStream().println(textResponse);
+		scanner.close();
 	}
 }
